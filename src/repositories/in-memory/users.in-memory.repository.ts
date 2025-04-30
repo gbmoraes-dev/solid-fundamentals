7a15dc434915d3f446aaf0b76b7857b5a19cd63b
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-30 18:47:41 -0300

    feat: ✨ add in-memory users repository implementation

---
+++ src/repositories/in-memory/users.in-memory.repository.ts
@@ -0,0 +1,31 @@
+import type { Prisma, User } from '@prisma/client'
+
+import type { UsersRepository } from '../users.repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: 'cm3i6qnaf0000cst58ckp06ej',
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      created_at: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
