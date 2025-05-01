8a8e9837cc3f94f9d4d618acc5a8c3a9fcd72cd0
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-01 02:34:45 -0300

    feat: ✨ add prisma check ins repository implementation

---
+++ src/repositories/prisma/check-ins.prisma.repository.ts
@@ -0,0 +1,37 @@
+import type { CheckIn, Prisma } from '@prisma/client'
+
+import { prisma } from '@/lib/prisma'
+
+import dayjs from 'dayjs'
+
+import type { CheckInsRepository } from '../check-ins.repository'
+
+export class PrismaCheckInsRepository implements CheckInsRepository {
+  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
+    const checkIn = await prisma.checkIn.create({
+      data,
+    })
+
+    return checkIn
+  }
+
+  async findByUserIdOnDate(
+    userId: string,
+    date: Date,
+  ): Promise<CheckIn | null> {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkIn = await prisma.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfTheDay.toDate(),
+          lte: endOfTheDay.toDate(),
+        },
+      },
+    })
+
+    return checkIn
+  }
+}
