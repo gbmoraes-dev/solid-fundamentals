d5b001af40514a5640edeae70b2fd69d60ab3627
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-01 02:35:46 -0300

    feat: ✨ add check in use case

---
+++ src/core/use-cases/check-ins/check-in.ts
@@ -0,0 +1,74 @@
+import type { CheckIn } from '@prisma/client'
+
+import type { CheckInsRepository } from '@/repositories/check-ins.repository'
+
+import type { GymsRepository } from '@/repositories/gyms.repository'
+
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
+
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found'
+
+import { MaxDistanceError } from '@/core/errors/max-distance'
+
+import { MaxNumberOfCheckInsError } from '@/core/errors/max-number-of-check-ins'
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+  userLatitude: number
+  userLongitude: number
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(
+    private readonly checkInsRepository: CheckInsRepository,
+    private readonly gymsRepository: GymsRepository,
+  ) {}
+
+  async execute({
+    userId,
+    gymId,
+    userLatitude,
+    userLongitude,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KM = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KM) {
+      throw new MaxDistanceError()
+    }
+
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDate) {
+      throw new MaxNumberOfCheckInsError()
+    }
+
+    const checkIn = await this.checkInsRepository.create({
+      user_id: userId,
+      gym_id: gymId,
+    })
+
+    return { checkIn }
+  }
+}
