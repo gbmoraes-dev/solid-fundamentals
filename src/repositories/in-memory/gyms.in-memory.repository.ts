81bc9403e51ffb8ebf0faec9424fb3e35d17a015
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-01 02:12:35 -0300

    feat: ✨ add in-memory users repository implementation

---
+++ src/repositories/in-memory/gyms.in-memory.repository.ts
@@ -0,0 +1,34 @@
+import { randomUUID } from 'node:crypto'
+
+import { type Gym, Prisma } from '@prisma/client'
+
+import type { GymsRepository } from '../gyms.repository'
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public gyms: Gym[] = []
+
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+    }
+
+    this.gyms.push(gym)
+
+    return gym
+  }
+
+  async findById(id: string): Promise<Gym | null> {
+    const gym = this.gyms.find((gym) => gym.id === id)
+
+    if (!gym) {
+      return null
+    }
+
+    return gym
+  }
+}
