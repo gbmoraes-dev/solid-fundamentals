c8f3b2e293a7a8a800ef82699662539d31fa83d7
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-12 18:18:06 -0300

    test: 🧪 add e2e tests for search gyms controller

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,57 @@
+import { afterAll, beforeAll, describe, expect, it, should } from 'vitest'
+
+import request from 'supertest'
+
+import { app } from '@/app'
+
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gyms e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms/register')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'SmartFit',
+        description: 'Maior rede de academias do Brasil.',
+        phone: null,
+        latitude: -22.9310464,
+        longitude: -43.3586176,
+      })
+
+    await request(app.server)
+      .post('/gyms/register')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'BodyTech',
+        description: 'Melhor rede de academias do Brasil.',
+        phone: null,
+        latitude: -22.9310464,
+        longitude: -43.3586176,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({ query: 'SmartFit' })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'SmartFit',
+      }),
+    ])
+  })
+})
