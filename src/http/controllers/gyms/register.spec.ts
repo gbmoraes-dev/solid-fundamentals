cc5e6910fca61811b4524274036947e3080b0e8c
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-12 18:17:31 -0300

    test: 🧪 add e2e tests for register gym controller

---
+++ src/http/controllers/gyms/register.spec.ts
@@ -0,0 +1,34 @@
+import { afterAll, beforeAll, describe, expect, it, should } from 'vitest'
+
+import request from 'supertest'
+
+import { app } from '@/app'
+
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Register Gym e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register a new gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms/register')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'SmartFit',
+        description: 'Maior rede de academias do Brasil.',
+        phone: null,
+        latitude: -22.9310464,
+        longitude: -43.3586176,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
