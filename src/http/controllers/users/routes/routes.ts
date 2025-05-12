6306a3c145f6601ee41eed06556a4eba45647492
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-12 19:33:17 -0300

    feat: ✨ add route for token refresh

--- src/http/controllers/users/routes/routes.ts
+++ src/http/controllers/users/routes/routes.ts
@@ -4,6 +4,8 @@ import { register } from '../register.controller'
 
 import { authenticate } from '../authenticate.controller'
 
+import { refresh } from '../refresh.controller'
+
 import { profile } from '../profile.controller'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt.middleware'
@@ -12,6 +14,8 @@ export async function usersRoutes(app: FastifyInstance) {
   app.post('/register', register)
 
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
