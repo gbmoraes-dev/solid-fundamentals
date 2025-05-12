395506b4af13aebde369f8f764c32f6b0e2431dc
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-12 17:19:02 -0300

    feat: ✨ add vitest environment setup for prisma testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,4 +4,25 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/core/use-cases',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment:
+            './prisma/vitest-enviroment-prisma/prisma-test-enviroment.ts',
+        },
+      },
+    ],
+  },
 })
