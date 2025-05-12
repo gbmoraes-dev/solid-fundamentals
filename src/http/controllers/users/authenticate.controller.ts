86703419949ea3fc2354858dde6e421f94d97167
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-12 17:21:19 -0300

    feat: ✨ add user authenticate controller

---
+++ src/http/controllers/users/authenticate.controller.ts
@@ -0,0 +1,45 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+import { z } from 'zod'
+
+import { makeAuthenticateUseCase } from '@/core/use-cases/users/factories/make-authenticate'
+
+import { InvalidCredentialsError } from '@/core/errors/invalid-credentials'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({ token })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    throw error
+  }
+}
