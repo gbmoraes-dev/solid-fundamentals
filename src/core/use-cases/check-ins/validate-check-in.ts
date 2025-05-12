bd0854a9fe3585a9a1624f460935c67951b60780
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-11 23:27:16 -0300

    feat: ✨ add validate check in use case

---
+++ src/core/use-cases/check-ins/validate-check-in.ts
@@ -0,0 +1,46 @@
+import type { CheckIn } from '@prisma/client'
+
+import dayjs from 'dayjs'
+
+import type { CheckInsRepository } from '@/repositories/check-ins.repository'
+
+import { LateCheckInValidationError } from '@/core/errors/late-check-in-validation'
+
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private readonly checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return { checkIn }
+  }
+}
