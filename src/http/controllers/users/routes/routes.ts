43eff94a50d665f308ea9380ca68ab6e9ab3edca
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-12 17:23:23 -0300

    feat: ✨ add user routes

---
+++ src/http/controllers/users/routes/routes.ts
@@ -0,0 +1,17 @@
+import type { FastifyInstance } from 'fastify'
+
+import { register } from '../register.controller'
+
+import { authenticate } from '../authenticate.controller'
+
+import { profile } from '../profile.controller'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt.middleware'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/register', register)
+
+  app.post('/sessions', authenticate)
+
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
