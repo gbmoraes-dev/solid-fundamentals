c000f84fe8812ab92c88a95c8106527a0118d02e
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-05-12 17:20:19 -0300

    test: 🧪 add e2e tests for user register controller

---
+++ src/http/controllers/users/register.spec.ts
@@ -0,0 +1,25 @@
+import { afterAll, beforeAll, describe, expect, it, should } from 'vitest'
+
+import request from 'supertest'
+
+import { app } from '@/app'
+
+describe('Register e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register a new user', async () => {
+    const response = await request(app.server).post('/register').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
